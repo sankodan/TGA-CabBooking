30ea5bef1675d68e943c48700998825c7b69e34b
Sandeep Kumar <>
2022-07-26 12:53:45 +0530

    testing packages

---
+++ packages/riderapp/jest-setup-after-env.js
@@ -0,0 +1,6 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import '@testing-library/jest-native/extend-expect';
+
+configure({ adapter: new Adapter() });
+
---
+++ packages/riderapp/src/NewMessageForm.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { Pressable, Text, TextInput } from 'react-native';
+
+export default function NewMessageForm({ onSend }) {
+    const [inputText, setInputText] = useState('');
+
+    function handleSend() {
+        onSend(inputText);
+        setInputText('');
+    }
+
+    return (
+        <>
+            <TextInput
+                testID="messageText"
+                value={inputText}
+                onChangeText={setInputText}
+            />
+            <Pressable testID="sendButton" onPress={handleSend}>
+                <Text>Send</Text>
+            </Pressable>
+        </>
+    );
+}
