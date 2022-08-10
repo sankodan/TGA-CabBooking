f4564dea02fe94dec0d468b93a266876d3efc758
Sandeep Kumar <sandeep.kumar5@publicissapient.com>
2022-08-11 00:14:11 +0530

    Update emailValidator.js

--- packages/core/src/helpers/emailValidator.js
+++ packages/core/src/helpers/emailValidator.js
@@ -1,5 +1,5 @@
 export function emailValidator (email) {
-  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  const re = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/
   if (!email) return "Email can't be empty."
   if (!re.test(email)) return 'Ooops! We need a valid email address.'
   return ''
