6bc10e659ecb9c2ac1fc8dd6eb006ca395cb9d5d
Sandeep Kumar <sandeep.kumar5@publicissapient.com>
2022-08-17 20:06:22 +0530

    Update Checkout.spec.js

--- packages/dashboard/src/screens/test/Checkout.spec.js
+++ packages/dashboard/src/screens/test/Checkout.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react-native'
-import Checkout from '../Checkout'
+import Checkout from '../CheckOut'
 import { shallow } from 'enzyme'
 
 describe('Checkout', () => {
